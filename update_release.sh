4834e49afa9b16eef76c615d38ae648a26497554
XatMassacrE <xxat.massacre@gmail.com>
2020-01-03 10:16:36 +0800

    Update fixed latest version tag

--- update_release.sh
+++ update_release.sh
@@ -1,4 +1,4 @@
-zip -r -o chainx-1.0.x.zip ../chainx-extension/packages/extension/build
+zip -r -o chainx-latest.zip ../chainx-extension/packages/extension/build
 git add .
 git commit -m 'Update release version'
 git push origin master
